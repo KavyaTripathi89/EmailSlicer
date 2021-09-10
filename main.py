ed92a3a87a6dcc6e47dc8b509ad47861d2b8cbb8
Kavya <kavyatripathi89@gmail.com>
2021-09-10 23:39:15 +0530

    email verified

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-# Taking user email id input
-email=input("Enter a valid email ID: ").strip()
-#Extracting user name
-user_name=email[:email.index("@")]
-#Extracting domain name
-doamin_name=email[email.index("@")+1:]
-#printing result
-result = "Your username is '{}' and your domain name is '{}' :".format(user_name,doamin_name)
+import re   
+regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'  
 
-print(result)
+def check():  
+    email = input("Enter a valid Email ID: ")
+    if(re.search(regex,email)): 
+        print("Valid Email") 
+        email.strip()
+        user_name=email[:email.index("@")]
+        doamin_name=email[email.index("@")+1:]
+        result = "Your username is '{}' and your domain name is '{}' :".format(user_name,doamin_name)
+        print(result)
+    else:   
+        print("Invalid Email") 
+        
+check()
+while input("\n Do you want to Check again 'yes' or 'no': ").lower()=='yes':
+    check()
+
+
